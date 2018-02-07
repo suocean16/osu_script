5b5ef660616758ca2b25fd129ba316f1cb39fa85
haiyang.su <haiyang.su@zyprotect.com>
2018-02-07 17:07:34 +0800

    update

--- zyproject/zytools/not_using/v4fa_alert_format.sh
+++ zyproject/zytools/not_using/v4fa_alert_format.sh
@@ -1,15 +1,13 @@
 #!/usr/bin/bash
 #res_path="`pwd`/attack_result"
+echo $0 "aaa" $1  "bbb" $2
 
-[ "x$3" = "x" ] && echo -e "Usage:\n\t$0 alert_folder output_folder script_folder"  && exit 0
+[ "x$2" = "x" ] && echo -e "Usage:\n\t$0 alert_folder output_folder"  && exit 0
 
-# result *.html is in here
 res_path="`pwd`"
+target_folder="./"
 
-# alert.xml in in here
-alert_folder="./"
-
-[ $2 != "" ] && res_path=${2} && alert_folder=${1} && script_folder=${3}
+[ $2 != "" ] && res_path=${2} && target_folder=${1}
 
 
 # find_attack option alert-msg.*ffff  escape
@@ -21,7 +19,7 @@ find_attack()
 	title=$4
 	outfile=${res_path}/${5}
 
-        find ${alert_folder} -name "alert*" \
+        find ${target_folder} -name "alert*" \
                 | xargs egrep  -h ${option} "${need}"\
                 | egrep -v "${escape}"  > ${outfile}
 
@@ -37,10 +35,10 @@ find_attack()
 }
 
 
-#find_attack "-A 1 -B 1" "alert-msg.*FCKeditor" "<none-need-escape>" "FCKeditor Attack" "fckeditor.html"
+find_attack "-A 1 -B 1" "alert-msg.*FCKeditor" "<none-need-escape>" "FCKeditor Attack" "fckeditor.html"
 find_attack "-A 1 -B 1" "alert-msg.*((org.apache.struts2)|(HttpServlet))" "<none-need-escape>" "Java Struts2 Attack" "java_struts2.html"
 find_attack "-A 1 -B 1" "alert-msg.*etc.+passwd" "<none-need-escape>" "Linux Password Attack" "etc_passwd.html"
-#find_attack "-A 1 -B 1" "alert-msg.*(\.asp\?|\.aspx\?)" "<none-need-escape>" "ASP Detect Attack" "asp_detect.html"
+find_attack "-A 1 -B 1" "alert-msg.*(\.asp\?|\.aspx\?)" "<none-need-escape>" "ASP Detect Attack" "asp_detect.html"
 find_attack "-B 4" "<request>PUT" "<remote-name>" "File upload Attack" "http_put.html"
 find_attack "-A 1 -B 1" "alert-msg.*www.agri-check.com" "<none-need-escape>" "Gambling Web Site" "gambling_agri.html"
 find_attack "-A 1 -B 1" "alert-msg.*download\.php\?open=" "<none-need-escape>" "PHP code injection" "php_code.html"
@@ -48,20 +46,7 @@ find_attack "-A 1 -B 1" "alert-msg.*(phpinfo|php\?)" "<none-need-escape>" "PHP d
 find_attack "-A 1 -B 1" "alert-msg.*method=screen.get.*timestamp=.*mode=2.*screenid" "<none-need-escape>" "Zabbix system detect" "zabbix.html"
 find_attack "-B 4" "<request>.*/.*((\.rar)|(\.zip)|(\.tar)|(tar\.gz)|(\.gz)) HTTP" "<remote-name>" "Get WebServer Left file" "fetch_compression.html"
 find_attack "-A 1 -B 1" "alert-msg.*((\.gitignore)|(\.git))" "<none-need-escape>" "Get left GIT file" "detect_git.html"
+#find_attack "-A 1 -B 1" "alert-msg.*select.*from" "<none-need-escape>" "SQL Injection" "SQL_injection.html"
 find_attack "-i -A 1 -B 1" "alert-msg.*select[%* ]" "<none-need-escape>" "SQL Injection" "SQL_injection.html"
 find_attack "-A 1 -B 1" "alert-msg.*((WindowsPowerShell)|(cmd\.exe))" "<none-need-escape>" "Windows system attack" "win_attack.html"
 find_attack "-B 4" "<request>(OPTIONS)|(PROPFIND)|(CONNECT)" "<remote-name>" "HTTP Method Attack" "http_method_attack.html"
-
-
-
-
-
-awk 'BEGIN{printf "egrep -v \""}; /<ip>/{gsub(/\t/,"");printf("%s|",$0)};END{printf("<ip><ip><ip><ip><ip>\"  ")}' ${res_path}/*.html > ${script_folder}/except_ip.sh
-
-echo -n "${alert_folder}/* " >> ${script_folder}/except_ip.sh
-echo -n " | fgrep -A 1 -B 1 \"<ip>\" "  >> ${script_folder}/except_ip.sh
-chmod a+x ${script_folder}/except_ip.sh
-#${script_folder}/except_ip.sh
-
-
-
